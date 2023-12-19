f838851283935269e38f0c555948ae6e819e8ce3
Jason Zhen <15258281+JasZhe@users.noreply.github.com>
2023-12-19 02:04:30 -0500

    kinda fixing the scrolling getting stuck

--- code-context.el
+++ code-context.el
@@ -30,6 +30,7 @@
   (find-previous-non-empty-line)
   (let ((ctx '())
         (prev-indentation (current-indentation)))
+    (cl-pushnew (buffer-substring-no-properties (line-beginning-position) (line-end-position)) ctx)
     (while (> (current-indentation) 0)
       (forward-line -1)
       (find-previous-non-empty-line)
@@ -40,31 +41,53 @@
       )
     ctx))
 
-;; TODO: move the save excursions into one parent save excursion maybe
+;x TODO: move the save excursions into one parent save excursion maybe
+;; (let ((replaced-char (buffer-substring 1473 1474)))
+;;   (setq test-ov (make-overlay 1473 1474))
+;;   (overlay-put test-ov 'display (propertize "hello\ngoodbye\nsdad"))
+;;   (overlay-put test-ov 'after-string replaced-char)
+;;   )
+;; (remove-overlays)
+
+
+(setq prev-ctx nil)
+;; issue where we can't keep scrolling if overlay would move cursor outside of scroll margin
+;; really only an issue when scrolling up
 (defun code-context-single-overlay (display-start)
   (remove-overlays (point-min) (point-max) 'name 'jason)
-
   (let ((ctx (save-excursion (get-context-from display-start))))
-    (when ctx
-      (let* (;; when window start is empty line ov-beg is display start
-             (display-start-empty-line-p (save-excursion (goto-char display-start) (looking-at-p "^$")))
-             (ol-beg-pos display-start ;;(if display-start-empty-line-p (save-excursion (goto-char display-start) (forward-line -1) (line-beginning-position)) display-start)
-             )
-             ;; when window start is empty line, ov-end is end of line after display-start
-             (ol-end-pos (if display-start-empty-line-p (save-excursion (goto-char display-start) (forward-line) (line-end-position)) (save-excursion (goto-char display-start) (line-end-position))))
-             (ol (make-overlay ol-beg-pos ol-end-pos))
-             ;; we need to add the last line into the overlay cause we cover it with the overlay only if the window start is not empty
-             (last-line (if display-start-empty-line-p
-                            (save-excursion (goto-char display-start) (forward-line) (buffer-substring (line-beginning-position) (line-end-position)))
-                          (save-excursion (goto-char display-start) (buffer-substring (line-beginning-position) (line-end-position)))))
-             (context-str-1 (when ctx (cl-reduce (lambda (acc str) (concat acc "\n" str)) ctx :initial-value "")))
-             (context-str (if display-start-empty-line-p context-str-1 context-str-1)))
-        (setq-local buffer-overlay ol)
-        (overlay-put buffer-overlay 'name 'jason)
-        (overlay-put buffer-overlay 'display (concat context-str "\n------------context------------\n" last-line))))))
+    (let* ((display-start-empty-line-p (save-excursion (goto-char display-start) (looking-at-p "^$")))
+           (ol-beg-pos display-start)
+           (ol-end-pos (save-excursion (goto-char display-start) (if display-start-empty-line-p (+ 1 display-start) (line-end-position))))
+           (ol (make-overlay ol-beg-pos ol-end-pos))
+           ;; we need to add the last line into the overlay cause we cover it with the overlay only if the window start is not empty
+           (replaced-char (char-to-string (char-after display-start)))
+           (context-str-1 (when ctx (cl-reduce (lambda (acc str) (concat acc "\n" str)) ctx)))
+           (context-str (if display-start-empty-line-p context-str-1 context-str-1)))
+
+      ;; this only seems to work with post-command-hook
+      (when (and ctx (or (eq last-command 'evil-scroll-line-up)
+                         (eq last-command 'scroll-down-line)))
+        (forward-line (- (+ (min (- (length ctx) (length prev-ctx)) 0) 1))))
+
+      (setq-local buffer-overlay ol)
+      (overlay-put buffer-overlay 'name 'jason)
+      (overlay-put buffer-overlay 'display context-str)
+      )
+    (setq prev-ctx ctx)))
 
 (defun code-context-window-scroll-function (_ display-start)
-  (when (buffer-file-name) (code-context-single-overlay (+ 1 display-start))))
+  (let ((display-start-empty-line-p (save-excursion (goto-char display-start) (looking-at-p "^$"))))
+    (when (buffer-file-name)
+      (code-context-single-overlay display-start)
+      )
+    )
+  )
+
+(advice-add 'redisplay :before (lambda () (code-context-window-scroll-function nil (window-start))))
+(advice-remove 'redisplay (lambda () (code-context-window-scroll-function nil (window-start))))
+
+(add-hook 'post-command-hook (lambda () (code-context-window-scroll-function nil (window-start))))
 
 ;;;###autoload
 (define-minor-mode code-context-mode
@@ -72,13 +95,23 @@
   :init-value nil
   :lighter " CodeCtx"
   (if code-context-mode
-      (add-to-list 'window-scroll-functions #'code-context-window-scroll-function)
+      (progn
+        (setq previous-window-start (window-start))
+        (add-to-list 'window-scroll-functions #'code-context-window-scroll-function))
     (progn
       (setq window-scroll-functions (remove 'code-context-window-scroll-function window-scroll-functions))
       (remove-overlays (point-min) (point-max) 'name 'jason))))
 
+(let ((x 1))
+  (progn
+    (progn
+      (progn
+        (progn
+          (progn ))))))
+
 (provide 'code-context)
 ;;; code-context.el ends here
+
 
 (defun test-function ()
   (when (buffer-file-name)
