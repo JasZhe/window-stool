3fe4d894bdbea9d67f0e2b2bac4156e3135b98fa
Jason Zhen <15258281+JasZhe@users.noreply.github.com>
2023-12-19 02:50:51 -0500

    use post command hook instead of window scroll functions
    have one buffer local overlay for each buffer

--- code-context.el
+++ code-context.el
@@ -41,40 +41,31 @@
       )
     ctx))
 
-;x TODO: move the save excursions into one parent save excursion maybe
-;; (let ((replaced-char (buffer-substring 1473 1474)))
-;;   (setq test-ov (make-overlay 1473 1474))
-;;   (overlay-put test-ov 'display (propertize "hello\ngoodbye\nsdad"))
-;;   (overlay-put test-ov 'after-string replaced-char)
-;;   )
-;; (remove-overlays)
-
-
 (setq prev-ctx nil)
+(unless buffer-overlay (setq-local buffer-overlay (make-overlay 1 1)))
 ;; issue where we can't keep scrolling if overlay would move cursor outside of scroll margin
 ;; really only an issue when scrolling up
 (defun code-context-single-overlay (display-start)
-  (remove-overlays (point-min) (point-max) 'name 'jason)
-  (let ((ctx (save-excursion (get-context-from display-start))))
-    (let* ((display-start-empty-line-p (save-excursion (goto-char display-start) (looking-at-p "^$")))
-           (ol-beg-pos display-start)
-           (ol-end-pos (save-excursion (goto-char display-start) (if display-start-empty-line-p (+ 1 display-start) (line-end-position))))
-           (ol (make-overlay ol-beg-pos ol-end-pos))
-           ;; we need to add the last line into the overlay cause we cover it with the overlay only if the window start is not empty
-           (replaced-char (char-to-string (char-after display-start)))
-           (context-str-1 (when ctx (cl-reduce (lambda (acc str) (concat acc "\n" str)) ctx)))
-           (context-str (if display-start-empty-line-p context-str-1 context-str-1)))
+  (when this-command
+    (let ((ctx (save-excursion (get-context-from display-start))))
+      (let* ((display-start-empty-line-p (save-excursion (goto-char display-start) (or (looking-at-p "^$") (looking-at-p "[[:blank:]]*$"))))
+             (ol-beg-pos display-start)
+             (ol-end-pos (save-excursion (goto-char display-start) (forward-line) (line-end-position)))
+             (covered-line (save-excursion (goto-char display-start) (forward-line) (buffer-substring (line-beginning-position) (line-end-position))))
+             (context-str-1 (when ctx (cl-reduce (lambda (acc str) (concat acc "\n" str)) ctx)))
+             (_ (set-text-properties 0 (length context-str-1) '(face hl-line) context-str-1))
+             (context-str (concat context-str-1 "\n-------------context-------------\n" covered-line)))
 
-      ;; this only seems to work with post-command-hook
-      (when (and ctx (or (eq last-command 'evil-scroll-line-up)
-                         (eq last-command 'scroll-down-line)))
-        (forward-line (- (+ (min (- (length ctx) (length prev-ctx)) 0) 1))))
+        ;; this only seems to work with post-command-hook
+        (when (and ctx (or (eq last-command 'evil-scroll-line-up)
+                           (eq last-command 'scroll-down-line)))
+          (forward-line (- (+ (min (- (length ctx) (length prev-ctx)) 0) 1))))
 
-      (setq-local buffer-overlay ol)
-      (overlay-put buffer-overlay 'name 'jason)
-      (overlay-put buffer-overlay 'display context-str)
-      )
-    (setq prev-ctx ctx)))
+        (move-overlay buffer-overlay ol-beg-pos ol-end-pos)
+        (overlay-put buffer-overlay 'name 'jason)
+        (overlay-put buffer-overlay 'display context-str)
+        )
+      (setq prev-ctx ctx))))
 
 (defun code-context-window-scroll-function (_ display-start)
   (let ((display-start-empty-line-p (save-excursion (goto-char display-start) (looking-at-p "^$"))))
@@ -84,11 +75,6 @@
     )
   )
 
-(advice-add 'redisplay :before (lambda () (code-context-window-scroll-function nil (window-start))))
-(advice-remove 'redisplay (lambda () (code-context-window-scroll-function nil (window-start))))
-
-(add-hook 'post-command-hook (lambda () (code-context-window-scroll-function nil (window-start))))
-
 ;;;###autoload
 (define-minor-mode code-context-mode
   "Minor mode to show code context based on indentation level within the buffer via overlays"
@@ -96,10 +82,9 @@
   :lighter " CodeCtx"
   (if code-context-mode
       (progn
-        (setq previous-window-start (window-start))
-        (add-to-list 'window-scroll-functions #'code-context-window-scroll-function))
+        (add-hook 'post-command-hook (lambda () (code-context-window-scroll-function nil (window-start)))))
     (progn
-      (setq window-scroll-functions (remove 'code-context-window-scroll-function window-scroll-functions))
+      (remove-hook 'post-command-hook (lambda () (code-context-window-scroll-function nil (window-start))))
       (remove-overlays (point-min) (point-max) 'name 'jason))))
 
 (let ((x 1))
