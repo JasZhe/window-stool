96a6dd865c1801a811a5059fccd526ef0a755105
Jason Zhen <15258281+JasZhe@users.noreply.github.com>
2024-02-13 02:32:59 -0500

    Fix some issues with doom/magit window rebalancing
    When quitting magit on a split frame in doom, the window rebalancing
    would be thrown off because of the window-stool-window.
    
    Killing the window, prior to showing the magit buffer via an advice to
    display-buffer seemed to fix it and should hopefully fix it for other
    issues as well.

--- window-stool-window.el
+++ window-stool-window.el
@@ -28,62 +28,58 @@
 (setq window-stool-window--buffer-name " *Window Stool*")
 
 (defun window-stool-window--create ()
-  (if (and (buffer-file-name)
-           (not (string-match "\\.git" (buffer-file-name)))
-           (not (eq (window-start) window-stool--prev-window-start)))
+  ;; only in file visiting buffers
+  (when (buffer-file-name)
     (let* ((ctx-1 (window-stool--truncate-context (save-excursion (funcall window-stool-fn (window-start)))))
            (ctx (if (not (eq major-mode 'org-mode)) (cl-subseq ctx-1 0 (1- (length ctx-1))) ctx-1))
            (buf-name window-stool-window--buffer-name)
            (buf (get-buffer-create buf-name))
            (win (display-buffer-in-direction buf '((direction . above)))))
 
+      (set-window-dedicated-p win t)
       (with-current-buffer buf
         (setq mode-line-format nil)
         (set-window-parameter win 'no-other-window t)
         (erase-buffer)
         (when ctx (insert (cl-reduce (lambda (acc str) (concat acc str)) ctx)))
-        (fit-window-to-buffer win)))
-    (window-stool-window--delete nil)))
+        (fit-window-to-buffer win)))))
 
 ;; call windmove up/down again if we switch into the code context window
 ;; to basically switch "past" it if we have two "real" windows on top of each other
-;; and rebalance
 (defun window-stool-window--windmove-up-advice ()
   (when (string= (buffer-name) window-stool-window--buffer-name)
-    (windmove-up)
-    )
-  )
+    (windmove-up)))
+
 (defun window-stool-window--windmove-down-advice ()
   (when (string= (buffer-name) window-stool-window--buffer-name)
-    (windmove-down))
-  )
+    (windmove-down)))
 
 (defun window-stool-window--split-window-advice (&optional _ _ _ _)
   (let ((win (get-buffer-window window-stool-window--buffer-name)))
-    (when win (delete-window win)))
-  )
+    (when win (delete-window win))))
 
-;; rebalance windows after deleting the code context one
-(defun window-stool-window--delete (_)
+(defun window-stool-window--delete (&rest _)
   (let ((win (get-buffer-window window-stool-window--buffer-name)))
     (when win (delete-window win))))
 
-
 (defun window-stool-window--advise-window-functions ()
+  ;; When quitting magit on a split frame in doom, the window rebalancing
+  ;; would be thrown off because of the window-stool-window.
+  ;;
+  ;; Killing the window, prior to showing the magit buffer via an advice to
+  ;; display-buffer seemed to fix it and should hopefully fix it for other
+  ;; issues as well.
+  (advice-add 'display-buffer :before #'window-stool-window--delete)
+
   (advice-add 'windmove-up :after #'window-stool-window--windmove-up-advice)
   (advice-add 'windmove-down :after #'window-stool-window--windmove-down-advice)
-  (advice-add 'split-window :before #'window-stool-window--split-window-advice)
-  ;; only allow the code context window to be active in the current window
-  (add-to-list 'window-selection-change-functions #'window-stool-window--delete)
-  )
+  (advice-add 'split-window :before #'window-stool-window--split-window-advice))
 
 (defun window-stool-window--remove-window-function-advice ()
+  (advice-remove 'display-buffer #'window-stool-window--delete)
   (advice-remove 'windmove-up #'window-stool-window--windmove-up-advice)
   (advice-remove 'windmove-down #'window-stool-window--windmove-down-advice)
-  (advice-remove 'split-window #'window-stool-window--split-window-advice)
-  (setq window-selection-change-functions (remove #'window-stool-window--delete window-selection-change-functions))
-  )
-
+  (advice-remove 'split-window #'window-stool-window--split-window-advice))
 
 (provide 'window-stool-window)
 ;;; window-stool-window.el ends here
