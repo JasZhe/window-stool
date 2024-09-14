f77d00a5c43b26178dca1f121d46bc5d2e903c20
Jason Zhen <15258281+JasZhe@users.noreply.github.com>
2024-09-13 20:03:40 -0400

    wip: tests

---
+++ tests/dummy.el
@@ -0,0 +1,90 @@
+;;; This file is used for interactively testing window-stool
+;;; Should not be evaluated
+
+(defun random-test-function ()
+  "This is a function."
+  (let ((one 1)
+        (two 2))
+    (progn
+      (when 1
+        (+ one two)
+        )
+      )
+    )
+  )
+
+;;; SPACER
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;;; SPACER so that this requires scrolling to get to
+
+(defun random-test-function2 ()
+  "This is another function."
+  (let ((one 1)
+        (two 2))
+    (progn
+      (when 1
+        (+ one two)
+        )
+      )
+    )
+  )
+
+;;; SPACER
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;;; SPACER
